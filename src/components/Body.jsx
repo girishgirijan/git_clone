85eb39bec0e376a825b60e7ff8bd04ee905bf7ab
Girish Girijan <girishvazheliparambil@gmail.com>
2024-06-08 11:56:49 -0300

    New section added in body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,6 +5,7 @@ const Body = () => {
     <>
       <div style={{"width": "200px", "height": "200px", "border": "solid 1px red"}}>NextJs</div>
       <div style={{"width": "200px", "height": "200px", "border": "solid 1px blue"}}>DRF</div>
+      <div style={{"width": "200px", "height": "200px", "border": "solid 1px blue"}}>Tailwind</div>
     </>
   )
 }
