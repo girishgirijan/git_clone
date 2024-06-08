4e72998c871d2e2245fb7d1e9ee4fb95ccdd2a64
Girish Girijan <girishvazheliparambil@gmail.com>
2024-06-08 11:32:07 -0300

    Body added

---
+++ src/components/Body.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Body = () => {
+  return (
+    <>
+      <div style={{"width": "200px", "height": "200px", "border": "solid 1px red"}}>NextJs</div>
+      <div style={{"width": "200px", "height": "200px", "border": "solid 1px blue"}}>DRF</div>
+    </>
+  )
+}
+
+export default Body
