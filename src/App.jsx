907d38be3cb71e467bc00bd76695673afe47a32e
Girish Girijan <girishvazheliparambil@gmail.com>
2024-06-08 11:51:49 -0300

    Footer added from girish-branch

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
 import Body from './components/Body'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
     <>
       <Header />
       <Body />
+      <Footer />
     </>
   )
 }
